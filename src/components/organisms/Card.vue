9513a66690deb909d0762ed1443871b07e099758
Eddine Djerboua <eddine.djerboua@gmail.com>
2018-04-30 23:18:57 +0200

    Creating base components

---
+++ src/components/organisms/Card.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="Card">
+    Cado
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="Home">
+    Wesh alors comme jul
+    <Card />
+  </div>
+</template>
+
+<script>
+import Card from '@/components/organisms/Card'
+export default {
+  name: 'Home',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    Card
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .Home {
+
+  }
+</style>
