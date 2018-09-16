99ba6c1188d06d1280e2ba49ed0d5c03da6478df
Eddine Djerboua <eddine.djerboua@gmail.com>
2018-09-16 23:16:58 +0200

    New Header Component

---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div class="Header">
+    <router-link :to="{ name: 'Home', params: {} }" class="Header__home">
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss">
+  .Header {
+    width: 100%;
+    height: 50px;
+    background: linear-gradient(25deg, #666, #252525);
+    display: flex;
+
+    &__home {
+      height: 100%;
+      width: 50px;
+      text-decoration: none;
+      color: black;
+
+      &::after {
+        content: '\2302';
+        font-size: 40px;
+      }
+    }
+  }
+</style>
